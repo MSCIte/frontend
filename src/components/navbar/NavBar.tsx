6c80e98ee3d68041a048359d89406ce4dc1250a6
Kevin Jiang <kevin51jiang at email.com>
2024-02-26 11:00:39 -0500

    get rid of help page, add instructions to landing page

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -8,14 +8,12 @@ export const Navbar = () => {
   const isAbout = useRoute("/about")[0];
   const isFAQ = useRoute("/faq")[0];
   const isDemo = useRoute("/demo")[0];
-  const isHelp = useRoute("/help")[0];
 
   const navigation = [
     { name: "Plan", href: "/plan", current: isPlan },
-    { name: "Help", href: "/help", current: isHelp },
     { name: "About MSCI Option", href: "/about", current: isAbout },
     { name: "FAQ", href: "/faq", current: isFAQ },
-    { name: "Component Playground", href: "/demo", current: isDemo },
+    // { name: "Component Playground", href: "/demo", current: isDemo },
   ];
 
   return (
